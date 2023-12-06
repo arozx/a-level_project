537827aff61ca44a0e1732ecac85ceb3c41792f8
arozx <github@arozx.tech>
2023-12-06 18:17:58 +0000

    rewrite for sqlite, add methods

--- dbConnector.py
+++ dbConnector.py
@@ -1,57 +1,185 @@
-import mysql.connector
+import sqlite3
 
 
 class DBConnector:
+    """
+    A class that represents a database connector.
+
+    Attributes:
+        host (str): The host of the database.
+        user (str): The username for the database.
+        password (str): The password for the database.
+        database (str): The name of the database.
+
+    Methods:
+        connect(): Connects to the database.
+        disconnect(): Disconnects from the database.
+        create_table(): Creates the necessary tables for the project.
+        display_previous_games(): Retrieves and returns all previous games from the database.
+        fetch_move_history(game_id): Retrieves and returns the move history for a specific game from the database.
+        delete_game(game_id): Deletes a game from the database.
+        find_game_rating(game_id): Retrieves and returns the rating of a specific game from the database.
+        find_player_rating(player_id): Retrieves and returns the rating of a specific player from the database.
+        insert_game(game_id, player_1_id, player_2_id, winner_id, rating): Inserts a new game into the database.
+        count_games(player_1_id, player_2_id): Counts the number of games between two players in the database.
+        calc_winrate(player_1_id): Calculates and returns the win rate of a specific player from the database.
+        longest_moves(player_id): Retrieves and returns the maximum move number for a specific player from the database.
+    """
+
     def __init__(self, host, user, password, database):
         self.host = host
         self.user = user
         self.password = password
         self.database = database
-        self.connection = None
 
     def connect(self):
-        self.connection = mysql.connector.connect(
-            host=self.host,
-            user=self.user,
-            password=self.password,
-            database=self.database,
-        )
+        """
+        Connects to the database.
+        """
+        self.connection = sqlite3.connect(self.database)
 
     def disconnect(self):
+        """
+        Disconnects from the database.
+        """
         if self.connection:
             self.connection.close()
 
-    def execute_query(self, query, params=None):
-        cursor = self.connection.cursor()
-        if params:
-            cursor.execute(query, params)
-        else:
-            cursor.execute(query)
-        self.connection.commit()
-        return cursor
+    # * NEA methods
 
-    def create_table(self, table_name, columns):
-        query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})"
-        self.execute_query(query)
+    def create_table(self):
+        """
+        Creates the necessary tables for the project.
+        """
+        self.create_table(
+            "Players",
+            "player_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, rating INTEGER",
+        )
+        self.create_table(
+            "Games",
+            "game_id INTEGER PRIMARY KEY AUTOINCREMENT, date_played TEXT, result INTEGER, player_1_id INTEGER, player_2_id INTEGER, rating INTEGER, player_1_rating INTEGER",
+        )
+        self.create_table(
+            "Moves",
+            "move_id INTEGER PRIMARY KEY AUTOINCREMENT, game_id INTEGER, player_id INTEGER, move_number INTEGER, move TEXT",
+        )
 
-    def insert_data(self, table_name, data):
-        columns = ", ".join(data.keys())
-        values = ", ".join(["%s"] * len(data))
-        query = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
-        self.execute_query(query, tuple(data.values()))
+    def display_previous_games(self):
+        """
+        Retrieves and returns all previous games from the database.
 
-    def select_data(self, table_name, condition=None):
-        query = f"SELECT * FROM {table_name}"
-        if condition:
-            query += f" WHERE {condition}"
+        Returns:
+            list: A list of tuples representing the previous games.
+        """
+        query = "SELECT * FROM games"
         cursor = self.execute_query(query)
         return cursor.fetchall()
 
-    def update_data(self, table_name, data, condition):
-        set_values = ", ".join([f"{column} = %s" for column in data.keys()])
-        query = f"UPDATE {table_name} SET {set_values} WHERE {condition}"
-        self.execute_query(query, tuple(data.values()))
+    def fetch_move_history(self, game_id):
+        """
+        Retrieves and returns the move history for a specific game from the database.
 
-    def delete_data(self, table_name, condition):
-        query = f"DELETE FROM {table_name} WHERE {condition}"
+        Args:
+            game_id (int): The ID of the game.
+
+        Returns:
+            list: A list of tuples representing the move history.
+        """
+        query = f"SELECT * FROM moves WHERE game_id = {game_id}"
+        cursor = self.execute_query(query)
+        return cursor.fetchall()
+
+    def delete_game(self, game_id):
+        """
+        Deletes a game from the database.
+
+        Args:
+            game_id (int): The ID of the game.
+        """
+        query = f"DELETE FROM games WHERE game_id = {game_id}"
         self.execute_query(query)
+
+    def find_game_rating(self, game_id):
+        """
+        Retrieves and returns the rating of a specific game from the database.
+
+        Args:
+            game_id (int): The ID of the game.
+
+        Returns:
+            list: A list of tuples representing the game rating.
+        """
+        query = f"SELECT rating FROM games WHERE game_id = {game_id}"
+        cursor = self.execute_query(query)
+        return cursor.fetchall()
+
+    def find_player_rating(self, player_id):
+        """
+        Retrieves and returns the rating of a specific player from the database.
+
+        Args:
+            player_id (int): The ID of the player.
+
+        Returns:
+            list: A list of tuples representing the player rating.
+        """
+        query = f"SELECT rating FROM players WHERE player_id = {player_id}"
+        cursor = self.execute_query(query)
+        return cursor.fetchall()
+
+    def insert_game(self, game_id, player_1_id, player_2_id, winner_id, rating):
+        """
+        Inserts a new game into the database.
+
+        Args:
+            game_id (int): The ID of the game.
+            player_1_id (int): The ID of player 1.
+            player_2_id (int): The ID of player 2.
+            winner_id (int): The ID of the winner.
+            rating (int): The rating of the game.
+        """
+        query = f"INSERT INTO games (game_id, player_1_id, player_2_id, winner_id, rating) VALUES ({game_id}, {player_1_id}, {player_2_id}, {winner_id}, {rating})"
+        self.execute_query(query)
+
+    def count_games(self, player_1_id, player_2_id):
+        """
+        Counts the number of games between two players in the database.
+
+        Args:
+            player_1_id (int): The ID of player 1.
+            player_2_id (int): The ID of player 2.
+
+        Returns:
+            list: A list of tuples representing the count of games.
+        """
+        query = f"SELECT COUNT(*) FROM Games WHERE player_1_id = {player_2_id} OR player_2_id = {player_1_id}"
+        cursor = self.execute_query(query)
+        return cursor.fetchall()
+
+    def calc_winrate(self, player_1_id):
+        """
+        Calculates and returns the win rate of a specific player from the database.
+
+        Args:
+            player_1_id (int): The ID of the player.
+
+        Returns:
+            list: A list of tuples representing the win rate.
+        """
+        query = f"SELECT COUNT(*) FROM games WHERE winner_id = {player_1_id}"
+        cursor = self.execute_query(query)
+        return cursor.fetchall()
+
+    def longest_moves(self, player_id):
+        """
+        Retrieves and returns the maximum move number for a specific player from the database.
+
+        Args:
+            player_id (int): The ID of the player.
+
+        Returns:
+            list: A list of tuples representing the maximum move number.
+        """
+        query = f"SELECT MAX(move_number) FROM moves WHERE player_id = {player_id}"
+        cursor = self.execute_query(query)
+        return cursor.fetchall()
--- test_dbConnector.py
+++ test_dbConnector.py
@@ -77,3 +77,82 @@ class TestDBConnector(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+import unittest
+from dbConnector import DBConnector
+
+
+class TestDBConnector(unittest.TestCase):
+    def setUp(self):
+        # Set up a test database connection
+        self.db = DBConnector(
+            host="localhost",
+            user="testuser",
+            password="testpassword",
+            database="testdb",
+        )
+        self.db.connect()
+
+    def tearDown(self):
+        # Disconnect from the test database
+        self.db.disconnect()
+
+    def test_create_table(self):
+        # Test creating a new table
+        table_name = "test_table"
+        columns = "id INT PRIMARY KEY, name VARCHAR(255), age INT"
+        self.db.create_table(table_name, columns)
+
+        # Check if the table exists
+        tables = self.db.execute_query("SHOW TABLES")
+        table_names = [table[0] for table in tables]
+        self.assertIn(table_name, table_names)
+
+    def test_insert_data(self):
+        # Test inserting data into a table
+        table_name = "test_table"
+        data = {"id": 1, "name": "John Doe", "age": 25}
+        self.db.insert_data(table_name, data)
+
+        # Check if the data was inserted correctly
+        result = self.db.select_data(table_name)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0][0], data["id"])
+        self.assertEqual(result[0][1], data["name"])
+        self.assertEqual(result[0][2], data["age"])
+
+    def test_select_data(self):
+        # Test selecting data from a table
+        table_name = "test_table"
+        condition = "age > 20"
+        result = self.db.select_data(table_name, condition)
+
+        # Check if the selected data meets the condition
+        for row in result:
+            self.assertGreater(row[2], 20)
+
+    def test_update_data(self):
+        # Test updating data in a table
+        table_name = "test_table"
+        condition = "id = 1"
+        data = {"name": "Jane Doe", "age": 30}
+        self.db.update_data(table_name, data, condition)
+
+        # Check if the data was updated correctly
+        result = self.db.select_data(table_name, condition)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0][1], data["name"])
+        self.assertEqual(result[0][2], data["age"])
+
+    def test_delete_data(self):
+        # Test deleting data from a table
+        table_name = "test_table"
+        condition = "id = 1"
+        self.db.delete_data(table_name, condition)
+
+        # Check if the data was deleted
+        result = self.db.select_data(table_name, condition)
+        self.assertEqual(len(result), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
