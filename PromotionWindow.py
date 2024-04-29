2106226de6b4e8717c0c34ba8ae221354ccff88a
arozx <jcooke@felsted.org>
2024-04-29 20:05:52 +0100

    fix issue calling window

--- PromotionWindow.py
+++ PromotionWindow.py
@@ -3,11 +3,11 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QGridLayout, QPushButton, QWidget
 
 
-# Promotion UI
 class PromotionWindow(QWidget):
+    pieceSelected = pyqtSignal(str)
+
     def __init__(self):
         super().__init__()
-        self.pieceSelected = pyqtSignal(str)
         self.initUI()
 
     def initUI(self):
@@ -17,19 +17,19 @@ class PromotionWindow(QWidget):
         # Create buttons for each piece
         queen_btn = QPushButton("Queen")
         queen_btn.setIcon(QIcon("media/white/Queen.svg"))
-        queen_btn.clicked.connect(lambda: self.piece_selected("Queen"))
+        queen_btn.clicked.connect(lambda: self.pieceSelected.emit("Queen"))
 
         rook_btn = QPushButton("Rook")
         rook_btn.setIcon(QIcon("media/white/Rook.svg"))
-        rook_btn.clicked.connect(lambda: self.piece_selected("Rook"))
+        rook_btn.clicked.connect(lambda: self.pieceSelected.emit("Rook"))
 
         bishop_btn = QPushButton("Bishop")
         bishop_btn.setIcon(QIcon("media/white/Bishop.svg"))
-        bishop_btn.clicked.connect(lambda: self.piece_selected("Bishop"))
+        bishop_btn.clicked.connect(lambda: self.pieceSelected.emit("Bishop"))
 
         knight_btn = QPushButton("Knight")
         knight_btn.setIcon(QIcon("media/white/Knight.svg"))
-        knight_btn.clicked.connect(lambda: self.piece_selected("Knight"))
+        knight_btn.clicked.connect(lambda: self.pieceSelected.emit("Knight"))
 
         # Add buttons to grid layout
         grid.addWidget(queen_btn, 0, 0)
@@ -40,9 +40,3 @@ class PromotionWindow(QWidget):
         self.setGeometry(100, 100, 200, 100)
         self.setWindowTitle("Promotion")
         self.show()
-
-    def piece_selected(self, piece):
-        self.close()
-        self.selected_piece = piece
-        self.pieceSelected.emit(piece)  # Emit the signal
-        return self.selected_piece
