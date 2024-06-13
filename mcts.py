fbc070597d25e0f9b34ec2d49a90cdc562ba9b08
arozx <jcooke@felsted.org>
2024-06-13 17:31:59 +0100

    implament alpha beta pruning

--- mcts.py
+++ mcts.py
@@ -76,9 +76,6 @@ class MCTS:
 
         print("self.all_valid_moves: ", all_valid_moves)  # Debug print
         self.root.expand(all_valid_moves)  # Expand root node with valid moves
-        print(
-            f"Root node has {len(self.root.children)} children after expansion."
-        )  # Debug print
 
         best_move = self.__call__(None)
 
@@ -92,33 +89,36 @@ class MCTS:
         while not node.is_leaf():
             node = node.best_child(exploration_weight=1.4)
             if node is None:
-                print("Node is None during iteration.")  # Debug print
                 return
 
         board = chess.Board(node.board_fen)  # Initialize board with FEN
         if not board.is_game_over():
             node.expand(self.all_valid_moves)
 
-        reward = self.negamax(board, depth=3, color=1)
+        reward = self.negamax(
+            board, depth=3, color=1, alpha=float("-inf"), beta=float("inf")
+        )
         while node is not None:
             node.update(reward)
             node = node.parent
 
-    def negamax(self, board, depth, color):
+    def negamax(self, board, depth, color, alpha, beta):
         if depth == 0 or board.is_game_over():
             return color * self.evaluate(board)
 
         max_value = float("-inf")
         for move in board.legal_moves:
             board.push(move)
-            value = -self.negamax(board, depth - 1, -color)
+            value = -self.negamax(board, depth - 1, -color, -beta, -alpha)
             board.pop()
             max_value = max(max_value, value)
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break  # Beta cutoff
 
         return max_value
 
     def evaluate(self, board):
-        # Simplified evaluation: material count
         material = sum(
             piece_value[piece.piece_type] for piece in board.piece_map().values()
         )
@@ -131,5 +131,5 @@ piece_value = {
     chess.BISHOP: 3,
     chess.ROOK: 5,
     chess.QUEEN: 9,
-    chess.KING: 0,  # The king's value is arbitrarily set to 0 since it cannot be captured
+    chess.KING: 0,  # The king's value = 0 since it cannot be captured
 }
